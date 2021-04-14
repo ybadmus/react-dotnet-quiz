95b2240757eb4da03a7e00919f8fb5cefec78983
ybadmus <yusif.badmus@gmail.com>
2021-04-14 11:38:11 +0800

    add answers and question models

---
+++ React.NET/Entities/Answer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace React.NET.Entities
+{
+    public class Answer
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [ForeignKey("AuthorId")]
+        public Question Question { get; set; }
+
+        public Guid QuestionId { get; set; }
+
+        [MaxLength(500)]
+        public string Text { get; set; }
+    }
+}
---
+++ React.NET/Entities/Question.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace React.NET.Entities
+{
+    public class Question
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string Text { get; set; }
+
+        public ICollection<Answer> Answers { get; set; }
+           = new List<Answer>();
+    }
+}
